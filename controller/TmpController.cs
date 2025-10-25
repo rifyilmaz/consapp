f33b76b74c892c31a2931090cd60f8254c70a10c
rifyilmaz <rifyilmaz@gmail.com>
2025-10-25 19:01:57 +0300

    conttroller eklendi

---
+++ controller/TmpController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace consapp.controller
+{
+    public class TmpController
+    {
+        
+    }
+}
